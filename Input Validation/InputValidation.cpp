50de3158a3f83625bca2d3ef29c44e9b11f0b7d3
eliseborbely <33642757+eliseborbely@users.noreply.github.com>
2017-12-08 21:48:00 -0500

    Elise Borbely
    Fixed my loop issue and put my finishing touches on the project. My sections are complete.

--- Input Validation/InputValidation.cpp
+++ Input Validation/InputValidation.cpp
@@ -2963,14 +2963,16 @@ void playSmallGame(string board[][5], string boardAnswer[][5], string boardTop, 
 		//--- reset check-input flag to false for next loop
 
 		//--- if user enters lowercase letter, this will change it to uppercase
-
-		while (valid == false && GiveUp == false)                                                      //--- check-input loop
+		if (GiveUp == false)
 		{
 			cout << endl << endl << lightmagenta << "Select a column (A-D) to enter a number in: ";     //--- ask user for a column
 			getline(cin, choiceCol);
 			choiceCol[0] = toupper(choiceCol[0]);
-			cout << lightcyan; 
+			cout << lightcyan;
+		}
 
+		while (valid == false && GiveUp == false)                                                      //--- check-input loop
+		{
 			if (choiceCol == "A" || choiceCol == "B" || choiceCol == "C" || choiceCol == "D" || choiceCol == "-999")  //--- if good input...
 			{
 				valid = true;                                                       //--- set flag to true
@@ -3006,15 +3008,15 @@ void playSmallGame(string board[][5], string boardAnswer[][5], string boardTop, 
 		}
 
 		valid = false;                                                              //--- flag to false again for next loop
-
-
-
-		while (valid == false && GiveUp == false)                                                      //--- check-input loop
+		if (GiveUp == false)
 		{
 			cout << endl << endl << lightmagenta << "Select a number (1-4) to insert in cell " << choiceCol << "-" << choiceRow << ": ";  //--- get a number to enter in chosen cell
 			getline(cin, choiceNum);
-			cout << lightcyan; 
+			cout << lightcyan;
+		}
 
+		while (valid == false && GiveUp == false)                                                      //--- check-input loop
+		{
 			if (choiceNum == "1" || choiceNum == "2" || choiceNum == "3" || choiceNum == "4" || choiceNum == "-999")  //--- if good input...
 			{
 				valid = true;                                                       //--- flag to true
